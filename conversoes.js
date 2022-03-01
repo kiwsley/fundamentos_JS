29d7894f6857d32f8f8efa41848a36bab04f8f9c
kiwsley <kiwsleyfreire@gmail.com>
2022-02-28 21:43:25 -0300

    condigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+
+//conversão implícita
+
+const numero = 456;
+const numeroString = '456a'
+
+console.log (numeroString === numero)
+console.log (numeroString == numero)
+
+console.log (Number(numeroString) + numero)
+
+//conversão explícita
+
---
+++ variaveis.js
@@ -0,0 +1,35 @@
+//var altura = 5;
+//var comprimento =7;
+//
+//area = altura*comprimento;
+//
+//console.log(area)
+//
+//var area;
+
+// let forma = 'retângulo'
+// let altura = 5
+// let comprimento = 7
+// let area
+
+// if(forma ===  'retângulo'){
+//     area = altura*comprimento;
+// }else{
+//     area = (altura * comprimento)/2
+// }
+
+const forma = 'triângulo'
+const altura = 5
+const comprimento =7
+let area;
+
+if(forma === 'Retângulo'){
+    area = altura*comprimento;
+}else{
+    area = (altura * comprimento)/2
+}
+
+
+
+console.log(area)
+
